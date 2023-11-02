fb9e807544a0ade19d2da1bdf2bf7b86bd061a87
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-11-02 19:53:17 +0100

    Added file browser

---
+++ Assets/Scripts/Test/FileBrowser.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileBrowser : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonsContainer;
+    [SerializeField]
+    private GameObject buttonPrefab;
+
+    private string currentPath = "C:\\";
+
+    void Start()
+    {
+        UpdateCurrentPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Music"));
+    }
+
+    public void UpdateCurrentPath(string newCurrentPath)
+    {
+        if (newCurrentPath == "C:")
+            newCurrentPath = "C:\\";
+
+        currentPath = newCurrentPath;
+
+        DeleteChildrenOfGameObject(buttonsContainer);
+
+        if (currentPath != "C:\\")
+        {
+            GameObject backDir = Instantiate(buttonPrefab, buttonsContainer.transform);
+            backDir.GetComponent<Button>().onClick.AddListener(() => UpdateCurrentPath(currentPath[..currentPath.LastIndexOf("\\")]));
+            backDir.GetComponentInChildren<Text>().text = "..";
+        }
+
+        foreach (string dir in Directory.GetDirectories(currentPath))
+        {
+            GameObject nextDir = Instantiate(buttonPrefab, buttonsContainer.transform);
+            nextDir.GetComponent<Button>().onClick.AddListener(() => UpdateCurrentPath(Path.Combine(currentPath, dir)));
+            nextDir.GetComponentInChildren<Text>().text = dir.Substring(dir.LastIndexOf("\\") + 1);
+        }
+        foreach (string file in Directory.GetFiles(currentPath))
+        {
+            if (file.EndsWith(".mp3") || file.EndsWith(".wav"))
+            {
+                GameObject nextFile = Instantiate(buttonPrefab, buttonsContainer.transform);
+                nextFile.GetComponentInChildren<Text>().text = file.Substring(file.LastIndexOf("\\") + 1);
+            }
+        }
+    }
+
+    private void DeleteChildrenOfGameObject(GameObject parent)
+    {
+        int childCount = parent.transform.childCount;
+
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            GameObject child = parent.transform.GetChild(i).gameObject;
+            Destroy(child);
+        }
+    }
+}
