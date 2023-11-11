e3dd48f9d5ee640838b1ce8123ae0cb07f4579d1
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-11-11 18:49:22 +0100

    Added blocks movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private struct BlockData
+    {
+        public Transform blockTransform;
+        public float endPercentage;
+        public float zPosition;
+    }
+
     [SerializeField]
     private AudioSource audioSource;
 
@@ -24,34 +31,66 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private GameObject blockPrefab;
+    [SerializeField]
+    private Material blockMaterial;
+
     private GameObject blockContainer;
-
     private BSpline trackSpline;
+    private List<BlockData> blocksData;
 
+    public bool IsGameRunning { get; private set; }
 
     void Awake()
     {
+        IsGameRunning = false;
+
         fileBrowser.AddOnAudioFileSelectedListener((songPath) => StartCoroutine(LoadAudioAndStartGame(songPath)));
         PlayerColorSyncher playerColorSyncher = playerController.GetComponent<PlayerColorSyncher>();
         colorSyncher.AddColorSynchable(playerColorSyncher);
         playerController.gameObject.SetActive(false);
         colorSyncher.enabled = false;
         pauseManager.enabled = false;
+
+        blocksData = new List<BlockData>();
     }
+
+    void Update()
+    {
+        if (!IsGameRunning)
+            return;
+
+        float currentPercentage = GetCurrentAudioTimePercentage();
+
+        blockMaterial.color = trackSpline.GetSplineColor(currentPercentage);
+
+        for (int i = 0; i < blocksData.Count; i++)
+        {
+            float finalPercentage = blocksData[i].endPercentage;
+            float currentBlockPositionPercentage = Mathf.Lerp(finalPercentage - 0.075f, finalPercentage,
+                    Mathf.InverseLerp(finalPercentage - 0.5f, finalPercentage, currentPercentage));
+
+            Vector3 position = trackSpline.GetSplinePoint(currentBlockPositionPercentage) + Vector3.forward * blocksData[i].zPosition;
+            Quaternion rotation = Quaternion.LookRotation(trackSpline.GetSplineTangent(currentBlockPositionPercentage), Vector3.up);
+
+            blocksData[i].blockTransform.parent.SetPositionAndRotation(position, rotation);
+        }
+    }
+
     public void BackToSelectMenu()
     {
+        IsGameRunning = false;
+
         selectFileUi.SetActive(true);
         colorSyncher.enabled = false;
         pauseManager.enabled = false;
         playerController.StopFollowinTrack();
         Destroy(blockContainer);
+        blocksData.Clear();
     }
 
     private IEnumerator LoadAudioAndStartGame(string songPath)
     {
         yield return StartCoroutine(AudioLoader.LoadAudio("file:\\\\" + songPath, audioSource));
-
-
 
         trackManager.GenerateTrack(audioSource.clip, 4096);
         trackSpline = trackManager.GetTrackSpline();
@@ -65,11 +104,12 @@ public class GameManager : MonoBehaviour
         foreach (int lowBeatIndex in lowBeatIndexes)
         {
             float percentage = trackSpline.GetSplinePercentageFromTrackIndex(lowBeatIndex + 4);
-            Instantiate(
+            Renderer blockRenderer = Instantiate(
                 blockPrefab,
                 trackSpline.GetSplinePoint(percentage) + Vector3.forward * ((lowBeatIndex + spawnLocationNoise) % 3 - 1) * 2.5f,
                 Quaternion.LookRotation(trackSpline.GetSplineTangent(percentage), Vector3.up),
-                blockContainer.transform).GetComponentInChildren<Renderer>().material.color = trackSpline.GetSplineColor(percentage);
+                blockContainer.transform).GetComponentInChildren<Renderer>();
+            blocksData.Add(new BlockData { blockTransform = blockRenderer.transform, endPercentage = percentage, zPosition = ((lowBeatIndex + spawnLocationNoise) % 3 - 1) * 2.5f });
 
             spawnLocationNoise++;
         }
@@ -81,14 +121,17 @@ public class GameManager : MonoBehaviour
                 continue;
 
             float percentage = trackSpline.GetSplinePercentageFromTrackIndex(highBeatIndex + 4);
-            Instantiate(
+            Renderer blockRenderer = Instantiate(
                 blockPrefab,
                 trackSpline.GetSplinePoint(percentage) + Vector3.forward * ((highBeatIndex + spawnLocationNoise) % 3 - 1) * 2.5f,
                 Quaternion.LookRotation(trackSpline.GetSplineTangent(percentage), Vector3.up),
-                blockContainer.transform).GetComponentInChildren<Renderer>().material.color = trackSpline.GetSplineColor(percentage); ;
+                blockContainer.transform).GetComponentInChildren<Renderer>();
+            blocksData.Add(new BlockData { blockTransform = blockRenderer.transform, endPercentage = percentage, zPosition = ((highBeatIndex + spawnLocationNoise) % 3 - 1) * 2.5f });
 
             spawnLocationNoise++;
         }
+
+        // Final setup
 
         selectFileUi.SetActive(false);
         playerController.SetNormalizedIntensities(trackManager.GetNormalizedIntensities());
@@ -98,6 +141,7 @@ public class GameManager : MonoBehaviour
         pauseManager.enabled = true;
         playerController.gameObject.SetActive(true);
         audioSource.Play();
+        IsGameRunning = true;
     }
 
     public float GetCurrentAudioTimePercentage()
