31481a3834330664c4bdbd1bfbd166d1a4525822
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-11-11 18:48:57 +0100

    Optimization on BSpline to avoid useless allocation (and GC)

--- Assets/Scripts/BSpline.cs
+++ Assets/Scripts/BSpline.cs
@@ -4,53 +4,77 @@ using UnityEngine;
 
 public class BSpline
 {
-    private List<Vector3> points;
-    private List<Color> colors;
+    private List<Vector3> splinePoints;
+    private List<Color> splineColors;
+
+    private float[,] bSplineMatrix;
+
+    private float distance;
+
+    // Cache for opimization purposes
+    private float[,] interpolators;
+    private float[,] points;
+    private float[,] res;
+
+    public BSpline()
+    {
+        interpolators = new float[1, 4];
+        points = new float[4, 3];
+        res = new float[4, 4];
+        PrecomputeBSplineMatrix();
+    }
 
     public void SetPoints(Vector3[] points)
     {
-        this.points = points.ToList<Vector3>();
+        splinePoints = points.ToList<Vector3>();
 
         // BSpline correction including first and last point
         Vector3 firstPoint = points[0];
         Vector3 lastPoint = points[^1];
 
-        this.points.Insert(0, firstPoint);
-        this.points.Insert(0, firstPoint);
-        this.points.Add(lastPoint);
-        this.points.Add(lastPoint);
+        distance = lastPoint.x - firstPoint.x;
+
+        splinePoints.Insert(0, firstPoint);
+        splinePoints.Insert(0, firstPoint);
+        splinePoints.Add(lastPoint);
+        splinePoints.Add(lastPoint);
     }
 
     public void SetColors(Color[] colors)
     {
-        this.colors = colors.ToList<Color>();
+        splineColors = colors.ToList<Color>();
 
         // BSpline correction including first and last point
         Color firstPoint = colors[0];
         Color lastPoint = colors[^1];
 
-        this.colors.Insert(0, firstPoint);
-        this.colors.Insert(0, firstPoint);
-        this.colors.Add(lastPoint);
-        this.colors.Add(lastPoint);
+        splineColors.Insert(0, firstPoint);
+        splineColors.Insert(0, firstPoint);
+        splineColors.Add(lastPoint);
+        splineColors.Add(lastPoint);
     }
 
     public Vector3 GetSplinePoint(float t)
     {
         GetSplineIndexes(in t, out int u, out float inter);
-        return GetPointOnSubSpline(inter, points[u], points[u + 1], points[u + 2], points[u + 3]);
+        return GetPointOnSubSpline(inter, splinePoints[u], splinePoints[u + 1], splinePoints[u + 2], splinePoints[u + 3]);
     }
 
     public Vector3 GetSplineTangent(float t)
     {
         GetSplineIndexes(in t, out int u, out float inter);
-        return GetTangentOnSubSpline(inter, points[u], points[u + 1], points[u + 2], points[u + 3]);
+        return GetTangentOnSubSpline(inter, splinePoints[u], splinePoints[u + 1], splinePoints[u + 2], splinePoints[u + 3]);
     }
 
     public Color GetSplineColor(float t)
     {
         GetSplineIndexes(in t, out int u, out float inter);
-        return Color.Lerp(colors[u], colors[u + 1], inter);
+        return Color.Lerp(splineColors[u], splineColors[u + 1], inter);
+    }
+
+    public float GetPercentageWithSpeedApplied(float percentage)
+    {
+        return GetSplinePoint(percentage).x / distance;
     }
 
     public Mesh GetSplineMesh(int resolution, float thickness, Vector3 bitangent)
@@ -122,21 +146,18 @@ public class BSpline
 
     public void GetSplineIndexes(in float t, out int u, out float inter)
     {
-        float lerp = Mathf.Lerp(0, points.Count - 4, t);
+        float lerp = Mathf.Lerp(0, splinePoints.Count - 4, t);
         u = (int)lerp;
         inter = lerp % 1;
     }
 
     public float GetSplinePercentageFromTrackIndex(int index)
     {
-        return Mathf.InverseLerp(0, points.Count - 4, index);
+        return Mathf.InverseLerp(0, splinePoints.Count - 4, index);
     }
 
     private Vector3 GetPointOnSubSpline(float t, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
     {
-        float[,] interpolators = new float[1, 4];
-        float[,] matrix = GetBSplineMatrix();
-        float[,] points = new float[4, 3];
         float bSplineMultiplier = 1f / 6f;
 
         interpolators[0, 0] = 1;
@@ -157,14 +178,12 @@ public class BSpline
         points[3, 1] = p4.y;
         points[3, 2] = p4.z;
 
-        float[,] result = MatrixMultiply(interpolators, MatrixMultiply(matrix, points));
+        float[,] result = MatrixMultiply(interpolators, MatrixMultiply(bSplineMatrix, points));
         return new Vector3(result[0, 0], result[0, 1], result[0, 2]) * bSplineMultiplier;
     }
+
     private Vector3 GetTangentOnSubSpline(float t, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
     {
-        float[,] interpolators = new float[1, 4];
-        float[,] matrix = GetBSplineMatrix();
-        float[,] points = new float[4, 3];
         float bSplineMultiplier = 1f / 6f;
 
         interpolators[0, 0] = 0;
@@ -185,56 +204,45 @@ public class BSpline
         points[3, 1] = p4.y;
         points[3, 2] = p4.z;
 
-        float[,] result = MatrixMultiply(interpolators, MatrixMultiply(matrix, points));
+        float[,] result = MatrixMultiply(interpolators, MatrixMultiply(bSplineMatrix, points));
         return new Vector3(result[0, 0], result[0, 1], result[0, 2]) * bSplineMultiplier;
     }
 
-    private float[,] GetBSplineMatrix()
+    private void PrecomputeBSplineMatrix()
     {
-        float[,] matrix = new float[4, 4];
-
-        matrix[0, 0] = 1;
-        matrix[0, 1] = 4;
-        matrix[0, 2] = 1;
-        matrix[0, 3] = 0;
-        matrix[1, 0] = -3;
-        matrix[1, 1] = 0;
-        matrix[1, 2] = 3;
-        matrix[1, 3] = 0;
-        matrix[2, 0] = 3;
-        matrix[2, 1] = -6;
-        matrix[2, 2] = 3;
-        matrix[2, 3] = 0;
-        matrix[3, 0] = -1;
-        matrix[3, 1] = 3;
-        matrix[3, 2] = -3;
-        matrix[3, 3] = 1;
-
-        return matrix;
+        bSplineMatrix = new float[4, 4];
+        bSplineMatrix[0, 0] = 1;
+        bSplineMatrix[0, 1] = 4;
+        bSplineMatrix[0, 2] = 1;
+        bSplineMatrix[0, 3] = 0;
+        bSplineMatrix[1, 0] = -3;
+        bSplineMatrix[1, 1] = 0;
+        bSplineMatrix[1, 2] = 3;
+        bSplineMatrix[1, 3] = 0;
+        bSplineMatrix[2, 0] = 3;
+        bSplineMatrix[2, 1] = -6;
+        bSplineMatrix[2, 2] = 3;
+        bSplineMatrix[2, 3] = 0;
+        bSplineMatrix[3, 0] = -1;
+        bSplineMatrix[3, 1] = 3;
+        bSplineMatrix[3, 2] = -3;
+        bSplineMatrix[3, 3] = 1;
     }
 
     private float[,] MatrixMultiply(float[,] matA, float[,] matB)
     {
-        float[,] res;
         int rowsA = matA.GetLength(0);
         int colsA = matA.GetLength(1);
-        int rowsB = matB.GetLength(0);
         int colsB = matB.GetLength(1);
 
-        if (colsA != rowsB)
-            res = null;
-        else
-        {
-            res = new float[rowsA, colsB];
-            for (int i = 0; i < colsB; i++)
-                for (int j = 0; j < rowsA; j++)
-                {
-                    float sum = 0;
-                    for (int k = 0; k < colsA; k++)
-                        sum += matA[j, k] * matB[k, i];
-                    res[j, i] = sum;
-                }
-        }
+        for (int i = 0; i < colsB; i++)
+            for (int j = 0; j < rowsA; j++)
+            {
+                float sum = 0;
+                for (int k = 0; k < colsA; k++)
+                    sum += matA[j, k] * matB[k, i];
+                res[j, i] = sum;
+            }
 
         return res;
     }
