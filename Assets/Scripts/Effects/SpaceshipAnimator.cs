b4ec7635c08dfcc5068983a85ed72210454ae1b4
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-11-29 11:30:44 +0100

    Fixed spaceship roll animation

--- Assets/Scripts/Effects/SpaceshipAnimator.cs
+++ Assets/Scripts/Effects/SpaceshipAnimator.cs
@@ -7,15 +7,13 @@ public class SpaceshipAnimator : MonoBehaviour
     [SerializeField] private float pitchFrequency = 1f;
     [SerializeField] private float floatingAmplitude = 0.2f;
     [SerializeField] private float floatingFrequency = 5f;
-    [SerializeField] private float rollMultiplier = 30000f;
     [SerializeField] private float maxRoll = 30f;
-    [SerializeField, Range(0, 1)] private float rollSmoothing = 0.975f;
+    [SerializeField, Tooltip("Degrees/s")] private float rollRotationSpeed = 30f;
 
     // Cache
     private float currentPitch;
     private float currentFloating;
     private float currentRoll;
-    private float mouseX;
 
     void Update()
     {
@@ -27,11 +25,16 @@ public class SpaceshipAnimator : MonoBehaviour
     /// </summary>
     private void UpdateSpaceshipPositionAndRotation()
     {
-        mouseX = -Input.GetAxis("Mouse X");
-
         currentPitch = pitchAmplitude * (Mathf.Sin(pitchFrequency * Time.time) * 0.5f + 0.5f);
         currentFloating = floatingAmplitude * (Mathf.Sin(floatingFrequency * Time.time) * 0.5f + 0.5f);
-        currentRoll = Mathf.Lerp(mouseX * Time.deltaTime * rollMultiplier, currentRoll, rollSmoothing);
+
+        currentRoll += -Input.GetAxis("Mouse X") * Time.deltaTime * rollRotationSpeed;
+
+        if (currentRoll > 1)
+            currentRoll -= rollRotationSpeed / 3.5f * Time.deltaTime;
+        if (currentRoll < -1)
+            currentRoll += rollRotationSpeed / 3.5f * Time.deltaTime;
+
         currentRoll = Mathf.Clamp(currentRoll, -maxRoll, maxRoll);
 
         transform.SetLocalPositionAndRotation(
