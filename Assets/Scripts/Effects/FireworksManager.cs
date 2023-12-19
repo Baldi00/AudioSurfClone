076be92b4655bf208e2ad13d85837525138fcb66
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-12-19 12:20:36 +0100

    Fireworks now adapts to the screen size (landscape vs portrait)

--- Assets/Scripts/Effects/FireworksManager.cs
+++ Assets/Scripts/Effects/FireworksManager.cs
@@ -7,6 +7,33 @@ public class FireworksManager : MonoBehaviour
     [SerializeField] private List<ParticleSystem> centerFireworks;
     [SerializeField] private List<ParticleSystem> rightFireworks;
 
+    [SerializeField] private float fireworksDurationPortrait;
+    [SerializeField] private Vector2 startSizePortrait;
+    [SerializeField] private float fireworksDurationLandscape;
+    [SerializeField] private Vector2 startSizeLandscape;
+
+    void Awake()
+    {
+        var particleSystems = new List<ParticleSystem>();
+        particleSystems.AddRange(leftFireworks);
+        particleSystems.AddRange(centerFireworks);
+        particleSystems.AddRange(rightFireworks);
+
+        var fireworksDuration = Screen.width > Screen.height ? fireworksDurationLandscape : fireworksDurationPortrait;
+        var startSize = Screen.width > Screen.height ? startSizeLandscape : startSizePortrait;
+
+        foreach (var particleSystem in particleSystems)
+        {
+            var mainModule = particleSystem.main;
+            mainModule.duration = fireworksDuration;
+            mainModule.startLifetime = fireworksDuration;
+            var size = mainModule.startSize;
+            size.constantMin = startSize.x;
+            size.constantMax = startSize.y;
+            mainModule.startSize = size;
+        }
+    }
+
     /// <summary>
     /// Emits fireworks at the given block position
     /// </summary>
